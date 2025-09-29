da8565371fb2da15fe2789a606be6a70a4d5de91
Gabi Socolovsky <gabisoco@post.bgu.ac.il>
2025-09-29 19:31:42 +0300

    Figure2a_Simulation completed

--- scripts/figure8/Figure8_Simulation.m
+++ scripts/figure8/Figure8_Simulation.m
@@ -28,7 +28,7 @@ N_e=1; % It is the same for analysing the order parameters in small fluctuations
 N_i=1;
 
 alpha=0.98; % relative depression %%
-muie=0.01;% measure of linearity
+muie=0.01; % measure of linearity
 muee=0.02;
 Jiemax=20; % J_ie_max
 Jeemax=1;
